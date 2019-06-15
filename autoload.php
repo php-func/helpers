23dc03a54da8e526dd3dbcbe53b56ca2b1b814d0
tom <tom@sapletta.com>
2019-06-15 15:39:30 +0200

    countries

--- autoload.php
+++ autoload.php
@@ -26,4 +26,5 @@ require_once($basePath . 'DictionaryCollection.php');
 require_once($basePath . 'Language.php');
 require_once($basePath . 'Definition.php');
 require_once($basePath . 'DefinitionCollection.php');
+require_once($basePath . 'ArrayFind.php');
 require_once($basePath . 'CountryCode.php');
